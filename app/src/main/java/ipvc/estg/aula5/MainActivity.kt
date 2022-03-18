a186d69ce4f76f43d419eb3f35497a92860ecb25
hugocosta18 <82047791+hugocosta18@users.noreply.github.com>
2022-03-18 11:47:55 +0000

    aula5

---
+++ app/src/main/java/ipvc/estg/aula5/MainActivity.kt
@@ -0,0 +1,27 @@
+package ipvc.estg.aula5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun changeName(view: View){
+        val editText = findViewById<EditText>(R.id.et1)
+        val textView=findViewById<TextView>(R.id.tv1)
+
+        if(editText.text.toString()=="") {
+            Toast.makeText(this, "please, write your name!", Toast.LENGTH_SHORT).show()
+        }else {
+            textView.setText(editText.text)
+        }
+
+    }
+}
