1c2c15c5dd82d23d837f26733a4952343e0d12a6
hugocosta18 <82047791+hugocosta18@users.noreply.github.com>
2022-03-18 11:57:06 +0000

    aula5

--- app/src/main/java/ipvc/estg/aula5/MainActivity.kt
+++ app/src/main/java/ipvc/estg/aula5/MainActivity.kt
@@ -1,5 +1,6 @@
 package ipvc.estg.aula5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -22,6 +23,16 @@ class MainActivity : AppCompatActivity() {
         }else {
             textView.setText(editText.text)
         }
+
+    }
+
+    fun goToNextPage(view: View) {
+        val intent = Intent(this, DashboardActivity::class.java)
+        startActivity(intent)
+
+
+
+
 
     }
 }
